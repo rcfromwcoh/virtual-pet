ceb26865b2d0a85afbb979fd4a4a48909f0e8ef3
rhorrocks <tipphorrocks@msn.com>
2016-07-31 12:58:21 -0400

    Adding code for program and virtual pet classes

--- VirtualPet/Program.cs
+++ VirtualPet/Program.cs
@@ -10,7 +10,59 @@ namespace VirtualPet
     {
         static void Main(string[] args)
         {
-            
+            VirtualPet grumpyBear = new VirtualPet();
+            string something;
+            string bearName;
+            Console.WriteLine("Welcome to the Grumpy Bear App! What do you want to name your bear? ");
+            bearName = Console.ReadLine();
+            int typeOfBear;
+            Console.WriteLine("What kind of bear do you want to be?");
+            Console.WriteLine("Press 1 for Polar, Press 2 for Grizzly, Press 3 for Panda, or Press 4 for Koala");
+            typeOfBear = int.Parse(Console.ReadLine());
+            do
+            {
+                grumpyBear.Tick();
+                grumpyBear.PrintStats();
+                Console.WriteLine("What do you want to do? Earn Money, Drink Beer, Eat, or Sleep?");
+                Console.WriteLine("Press 1 to earn money, 2 to drink beer, 3 to eat, or 4 to sleep");
+                string answer = Console.ReadLine();
+                if (answer == "1")
+                {
+                    grumpyBear.Raise();
+
+                }
+
+                else if (answer == "2")
+                {
+                    grumpyBear.DrinkBeer();
+                }
+
+                else if (answer == "3")
+                {
+                    grumpyBear.Eat();
+                }
+
+                else if (answer == "4")
+                {
+                    grumpyBear.Sleep();
+                }
+
+                else
+                {
+                    grumpyBear.DrinkBeer();
+
+                }
+
+                grumpyBear.PrintStats();
+
+                Console.WriteLine("Do you want to go again?");
+                something = Console.ReadLine();
+                something = something.ToUpper();
+                something = something.Substring(0, 1);
+
+            } while (something == "Y");
+            Console.ReadKey();
+
 
         }
     }
--- VirtualPet/VirtualPet.cs
+++ VirtualPet/VirtualPet.cs
@@ -22,7 +22,111 @@ namespace VirtualPet
         //          a. Sleep
         //          b. Poop
         //          c. Drink Beer
+        //Fields
+        private string typeOfBear;
+        private int money;
+        private int hunger;
+        private int happiness;
+        private int sleepiness;
+        private string name;
+        private string bearName;
 
-        //The Tick method will go here (in addition to the other methods)
+        //Properties
+        public string TypeOfBear
+        {
+            get { return this.typeOfBear; }
+            set { this.typeOfBear = value; }
+        }
+        public int Money
+        {
+            get { return this.money; }
+            set { this.money = value; }
+        }
+        public int Hunger
+        {
+            get { return this.hunger; }
+            set { this.hunger = value; }
+        }
+
+        public int Happiness
+        {
+            get { return this.happiness; }
+            set { this.hunger = value; }
+        }
+        public string Name
+        {
+            get { return this.name; }
+            set { this.name = value; }
+        }
+
+        public int Sleepiness
+        {
+            get { return this.sleepiness; }
+            set { this.sleepiness = value; }
+        }
+
+        public string BearName
+        {
+            get { return this.bearName; }
+            set { this.bearName = value; }
+        }
+
+        //Constructors
+        public VirtualPet()
+        {
+            this.typeOfBear = "Grizzly";
+            this.money = 10000;
+            this.hunger = 50;
+            this.happiness = 20;
+            this.name = bearName;
+            this.sleepiness = 25;
+        }
+
+        //Methods
+
+        public void Raise()
+        {
+            money = money + 5000;
+        }
+
+        public void Eat()
+        {
+            hunger = hunger - 15;
+            money = money - 2000;
+            happiness = happiness + 10;
+        }
+
+        public void DrinkBeer()
+        {
+            money = money - 500;
+            happiness = happiness + 20;
+            sleepiness = sleepiness + 25;
+        }
+
+        public void Sleep()
+        {
+            sleepiness = sleepiness - 50;
+            hunger = hunger + 20;
+            happiness = happiness + 15;
+        }
+
+        public void PrintStats()
+        {
+            Console.WriteLine("Your Grumpy Bear's Name is: " + this.name);
+            Console.WriteLine("Money: " + this.money);
+            Console.WriteLine("Hunger: " + this.hunger);
+            Console.WriteLine("Happiness: " + this.happiness);
+            Console.WriteLine("Sleepiness: " + this.sleepiness);
+        }
+
+        public void Tick()
+        {
+            money = money - 1000;
+            hunger = hunger + 5;
+            happiness = happiness - 5;
+            sleepiness = sleepiness + 10;
+
+        }
+        
     }
 }
